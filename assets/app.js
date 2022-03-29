e93be1e58592d995cb31b428fa5c707265a10631
Kevin <kevindelaunay74@gmail.com>
2022-03-29 09:37:19 +0200

    Add bootstrap

--- assets/app.js
+++ assets/app.js
@@ -7,6 +7,10 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
+// import bootstrap js
+import * as bootstrap from 'bootstrap';
+
+
 
 // start the Stimulus application
 import './bootstrap';
