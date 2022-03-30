d180d8e45c42075db630c4b9c1eba86d0605202d
Kevin <kevindelaunay74@gmail.com>
2022-03-30 10:37:01 +0200

    Add first four entities

---
+++ src/Entity/Etablissement.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EtablissementRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: EtablissementRepository::class)]
+class Etablissement
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 80)]
+    private $nom;
+
+    #[ORM\Column(type: 'string', length: 250)]
+    private $description;
+
+    #[ORM\Column(type: 'string', length: 160)]
+    private $adresse;
+
+    #[ORM\Column(type: 'string', length: 60)]
+    private $ville;
+
+    #[ORM\Column(type: 'integer')]
+    private $code_postale;
+
+    #[ORM\OneToMany(mappedBy: 'id_hotel', targetEntity: Suite::class)]
+    private $suites;
+
+    public function __construct()
+    {
+        $this->suites = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getAdresse(): ?string
+    {
+        return $this->adresse;
+    }
+
+    public function setAdresse(string $adresse): self
+    {
+        $this->adresse = $adresse;
+
+        return $this;
+    }
+
+    public function getVille(): ?string
+    {
+        return $this->ville;
+    }
+
+    public function setVille(string $ville): self
+    {
+        $this->ville = $ville;
+
+        return $this;
+    }
+
+    public function getCodePostale(): ?int
+    {
+        return $this->code_postale;
+    }
+
+    public function setCodePostale(int $code_postale): self
+    {
+        $this->code_postale = $code_postale;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Suite>
+     */
+    public function getSuites(): Collection
+    {
+        return $this->suites;
+    }
+
+    public function addSuite(Suite $suite): self
+    {
+        if (!$this->suites->contains($suite)) {
+            $this->suites[] = $suite;
+            $suite->setIdHotel($this);
+        }
+
+        return $this;
+    }
+
+    public function removeSuite(Suite $suite): self
+    {
+        if ($this->suites->removeElement($suite)) {
+            // set the owning side to null (unless already changed)
+            if ($suite->getIdHotel() === $this) {
+                $suite->setIdHotel(null);
+            }
+        }
+
+        return $this;
+    }
+}
