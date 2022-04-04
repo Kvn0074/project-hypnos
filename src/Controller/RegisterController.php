bfc7000e6fff86431eb656e357553ff21754dcad
Kevin <kevindelaunay74@gmail.com>
2022-04-04 17:32:07 +0200

    Add Form

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -2,17 +2,47 @@
 
 namespace App\Controller;
 
+use App\Entity\Compte;
+use App\Form\RegisterType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class RegisterController extends AbstractController
 {
-    #[Route('/register', name: 'app_register')]
-    public function index(): Response
+
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
     {
-        return $this->render('register/index.html.twig', [
-            'controller_name' => 'RegisterController',
+        $this->EntityManager = $entityManager;
+    }
+
+    #[Route('/register', name: 'app_register')]
+    public function index(Request $request, UserPasswordHasherInterface $hasher): Response
+    {
+        $compte = new Compte();
+        $form = $this->createForm(RegisterType::class, $compte);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+
+            $compte = $form->getData();
+
+            $password = $hasher->hashPassword($compte, $compte->getPassword());
+
+            $compte->setPassword($password);
+
+            $this->EntityManager->persist($compte);
+            $this->EntityManager->flush();
+
+        }
+        return $this->render('register/index.html.twig',[
+            'form' => $form->createView()
         ]);
     }
 }
---
+++ src/Form/RegisterType.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Compte;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+
+class RegisterType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('prenom', TextType::class, [
+                'label' => 'prenom',
+                'constraints'=> new Length(null, 2, 30),
+                'attr' => [
+                    'placeholder' => "votre prenom",
+                    'class' => "form-items"
+                ]
+            ])
+            ->add('nom', TextType::class, [
+                'label' => 'nom',
+                'constraints'=> new Length(null, 2, 30),
+                'attr' => [
+                    'placeholder' => "votre nom",
+                    'class' => "form-items"
+                ]
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'mail',
+                'constraints'=> new Length(null, 2, 60),
+                'attr' => [
+                    'placeholder' => "votre mail",
+                    'class' => "form-items"
+                ]
+            ])
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'les mots de passes doivent être identique' ,
+                'label' => 'votre mot de passe',
+                'required' => true,
+                'first_options' => [
+                    'label' => 'Mot de passe',
+                    'attr' => [
+                        'placeholder' => 'mot de passe',
+                        'class' => "form-items"
+                    ]
+                ],
+                'second_options' => [
+                    'label' => 'Confirmation Mot de passe',
+                    'attr' => [
+                        'placeholder' => 'confirmation mot de passe',
+                        'class' => "form-items"
+                    ]
+                ],
+
+            ])
+
+
+            ->add('submit', SubmitType::class, [
+                'label' => "S'inscrire",
+                'attr' => [
+                    'class' => "form-button"
+                ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Compte::class,
+        ]);
+    }
+}
