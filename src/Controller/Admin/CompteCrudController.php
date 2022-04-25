84cccc1be5934748ab94cb96bff0b9992c19171b
Kevin <kevindelaunay74@gmail.com>
2022-04-25 19:14:26 +0200

    CompteCrudController

--- src/Controller/Admin/CompteCrudController.php
+++ src/Controller/Admin/CompteCrudController.php
@@ -3,7 +3,12 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Compte;
+use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class CompteCrudController extends AbstractCrudController
 {
@@ -12,14 +17,47 @@ class CompteCrudController extends AbstractCrudController
         return Compte::class;
     }
 
-    /*
+
+    private $hasher;
+
+    public function __construct(UserPasswordHasherInterface $hasher)
+    {
+        $this->hasher = $hasher;
+    }
+
+
     public function configureFields(string $pageName): iterable
     {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
+        $nom = TextField::new('nom');
+        $prenom = TextField::new('prenom');
+        $email = EmailField::new('email');
+        $password = TextField::new('password')
+            ->setFormType(PasswordType::class);
+
+
+        return [$nom, $prenom, $email, $password];
     }
-    */
+
+    public function persistEntity(EntityManagerInterface $entityManager, $entityInstance): void
+    {
+        $this->encodePassword($entityInstance);
+        parent::persistEntity($entityManager, $entityInstance);
+    }
+
+    public function updateEntity(EntityManagerInterface $entityManager, $entityInstance): void
+    {
+        $this->encodePassword($entityInstance);
+        parent::updateEntity($entityManager, $entityInstance);
+    }
+
+    private function encodePassword(Compte $compte)
+    {
+        if ($compte->getPassword() !== null) {
+            $password = $this->hasher->hashPassword($compte, $compte->getPassword());
+
+            $compte->setPassword($password);
+
+        }
+
+    }
 }
